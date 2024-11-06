f01653c772b238057045e418688e8f30d42dae7d
Adam_01 <cks.adamkotwal@gmail.com>
2024-11-06 18:37:40 +0530

    ##11/06 Setting up Routes and Controllers

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const connectToMongoDB = require('./connect_db.js');
 const express = require('express');
 const post = 3001;
 const app = express();
+// const 
+const connectToMongoDB = require('./connect_db.js');
 
 connectToMongoDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+//Routes
+// app.use('/api/auth', );
 
 app.listen(post, () => {
     console.log(`Example app listening on port ${post}!`);
